db5d5a7d71e6be243988fb5d5f1e3a8c87c7d785
Darius <dariuskrtn@gmail.com>
2018-10-03 19:14:23 +0300

    Simple console logger

---
+++ SafePlace/Service/ConsoleLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafePlace.Service
+{
+    class ConsoleLogger : ILogger
+    {
+
+        public void LogInfo(string message)
+        {
+            Log("Info: " + message);
+        }
+        public void LogWarning(string message)
+        {
+            Log("Warning: " + message);
+        }
+        public void LogError(string message)
+        {
+            Log("Error: " + message);
+        }
+
+        private void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ SafePlace/Service/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafePlace.Service
+{
+    public interface ILogger
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+    }
+}
