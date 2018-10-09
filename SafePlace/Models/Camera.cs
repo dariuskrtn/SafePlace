84852d3a412ea4f2ef93f45d49fad7e939be7c3f
Tristanas <43069390+Tristanas@users.noreply.github.com>
2018-10-09 21:17:29 +0300

    Model improvements
    Added a new class Floor for convient switching between floors. Made Camera class inherit System.Windows.Control.Image, so that loading adding cameras to the FloorMap ViewBox element can later be implemented. And added various TODO comments.

--- SafePlace/Models/Camera.cs
+++ SafePlace/Models/Camera.cs
@@ -5,18 +5,62 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 /// <summary>
 /// A class which represents any camera in a office building.
 /// </summary>
 namespace SafePlace.Models
 {
-    class Camera
+    class Camera : System.Windows.Controls.Image
     {
-        public int floor;
+        #region Fields
+        //url for the image
+        private const string url = "/Images/camera.png";
+        public int Floor { set; get; }
+        
+        //IdentifiedPeople will be used in the list, shown near a camera.
+        private List<Person> IdentifiedPeople;
         //Following 2 might be bad practice if we create a separate class which represents an icon of a camera in the UI
         public double X; 
         public double Y;
+        
+        // It would be simpler to have 2 icons near a camera: a green one and a red one. Both could have a number of spotted people.
+        // They would appear depending on below 2 values.
+        public bool SpottedTrespasser;
+        public bool SpottedLegitPerson;
+        #endregion
+        //Need fields for camera footage and connection handling
 
-        //Need fields for connection and footage retrieval/display purposes
+        #region Constructors
+        public Camera(int floor)
+        {
+            this.Source = new BitmapImage(new Uri(url, UriKind.Relative));
+            Floor = floor;
+        }
+
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// This method adds a person to the list of recognised people. A person should be displayed near
+        /// </summary>
+        /// <param name="recognisedPerson"></param>
+        public void AddPerson(Person recognisedPerson)
+        {
+            //Perhaps additional checking should be added to prevent various weird situations, such as 2 cameras constantly removing same people from each other
+            if (recognisedPerson == null) return;
+            if (recognisedPerson.Camera != null)
+            {
+                recognisedPerson.Camera.RemovePerson(recognisedPerson);
+            }
+            IdentifiedPeople.Add(recognisedPerson);
+            recognisedPerson.Camera = this;
+        }
+        public void RemovePerson(Person person)
+        {
+            IdentifiedPeople.Remove(person);
+        }
+        #endregion
+
     }
 }
---
+++ SafePlace/Models/Floor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace SafePlace.Models
+{
+    class Floor
+    {
+        //What about the setters and getters?
+        List<Camera> cameras;
+        Image FloorMap { set; get; }
+        string FloorName { set; get; }
+        int FloorNumber;
+
+        //Creating a floor without any arguments should give it a placeholder image
+        public Floor()
+        {
+            var img = new BitmapImage(new Uri("/Images/placeholder.png", UriKind.Relative)); 
+            FloorMap = new Image();
+            FloorMap.Source = img;
+        }
+
+        //Could try finding the image with given url, if it cannot be found, default constructor will be used.
+        public Floor(string url, string name)
+        {
+            var img = new BitmapImage(new Uri(url, UriKind.Relative));
+            FloorMap = new Image();
+            FloorMap.Source = img;
+            FloorName = name;
+        }
+
+        /// <summary>
+        /// A method which allows to add a camera to the list while setting up the floor.
+        /// </summary>
+        /// <param name="PlanningCamera">A dummy camera, put on a floor image in the floor planning window</param>
+        public void AddCamera(Image PlanningCamera)
+        {
+            Camera camera = new Camera(FloorNumber);
+            camera.Margin = PlanningCamera.Margin;
+            cameras.Add(camera);
+        }
+    }
+}
