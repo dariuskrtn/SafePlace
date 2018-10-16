f2945ce1841eec2a4cca3e4f860b354c1cbf6f9c
Tomas Mikna <TMikna@users.noreply.github.com>
2018-10-17 02:04:08 +0300

    OpenFileDialog implementation and selected image showing in settings page

--- SafePlace/Views/SettingsPageView/SettingsPagePresenter.cs
+++ SafePlace/Views/SettingsPageView/SettingsPagePresenter.cs
@@ -1,10 +1,14 @@
-﻿using SafePlace.Service;
+﻿using Microsoft.Win32;
+using SafePlace.Service;
+using SafePlace.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace SafePlace.Views.SettingsPageView
 {
@@ -20,6 +24,40 @@ namespace SafePlace.Views.SettingsPageView
             _viewModel = viewModel;
             _logger = logger;
             _synchronisationContext = syncgronizationContext;
+
+            BuildViewModel();
+        }
+
+        private void BuildViewModel()
+        {
+            _viewModel.FloorButtonClickCommand = new RelayCommand(e => FloorButtonClickedCommand());
+        }
+
+        #region Commands
+
+        private void FloorButtonClickedCommand()
+        {
+            SelectFile();
+        }
+
+        #endregion
+
+        //Open file dialog, select image and assign to _viewModel
+        private void SelectFile()
+        {
+            // Open file dialog   
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Title = "Open Image";
+            // Image filters  
+            openDialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+
+            if (openDialog.ShowDialog() == false)
+            {
+                // Maybe should throw exception and show message for user?
+                _logger.LogInfo(" Failed to open selected image");
+                return;
+            }
+            _viewModel.FloorImage = new BitmapImage(new Uri(openDialog.FileName));
         }
     }
 }
--- SafePlace/Views/SettingsPageView/SettingsPageViewModel.cs
+++ SafePlace/Views/SettingsPageView/SettingsPageViewModel.cs
@@ -4,11 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace SafePlace.Views.SettingsPageView
 {
     class SettingsPageViewModel : BaseViewModel
     {
 
+        private ICommand _floorButtonClickCommand;
+        public ICommand FloorButtonClickCommand
+        {
+            get
+            {
+                return _floorButtonClickCommand;
+            }
+            set
+            { 
+                _floorButtonClickCommand = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private BitmapImage _floorImage;
+        public BitmapImage FloorImage
+        {
+            get
+            {
+                return _floorImage;
+            }
+            set
+            {
+                _floorImage = value;
+                NotifyPropertyChanged();
+            }
+        }
     }
+
 }
