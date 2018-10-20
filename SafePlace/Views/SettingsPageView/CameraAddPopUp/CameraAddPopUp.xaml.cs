9cc6ff249cb393337b0994c6a4bfca5e662751ce
Tristanas <Tristanas@users.noreply.github.com>
2018-10-20 11:36:55 +0300

    Camera add pop up
    Half baked.

---
+++ SafePlace/Views/SettingsPageView/CameraAddPopUp/CameraAddPopUp.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SafePlace.Views
+{
+    /// <summary>
+    /// Interaction logic for CameraAddPopUp.xaml
+    /// </summary>
+    public partial class CameraAddPopUp : Window
+    {
+        public CameraAddPopUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SafePlace/Views/SettingsPageView/CameraAddPopUp/CameraAddPopUpViewModel.cs
@@ -0,0 +1,66 @@
+﻿using SafePlace.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafePlace.Views.CameraAddPopUp
+{
+    class CameraAddPopUpViewModel : BaseViewModel
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private string _IPAdress;
+        public string IPAdress
+        {
+            get
+            {
+                return _IPAdress;
+            }
+            set
+            {
+                _IPAdress = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int _positionX;
+        public int PositionX
+        {
+            get
+            {
+                return _positionX;
+            }
+            set
+            {
+                _positionX = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int _positionY;
+        public int PositionY
+        {
+            get
+            {
+                return _positionY;
+            }
+            set
+            {
+                _positionY = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+    }
+}
