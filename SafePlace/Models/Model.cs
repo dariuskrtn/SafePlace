dfe8e3e63200f656f34f965d5b6210a2864bc052
Tomas Mikna <TMikna@users.noreply.github.com>
2018-11-14 03:05:35 +0200

    Added model class for every model to extend
    Lets create generic methods for models

---
+++ SafePlace/Models/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafePlace.Models
+{
+    /// <summary>
+    /// Lets create generic methods for models
+    /// </summary>
+    public abstract class Model
+    {
+       
+    }
+}
