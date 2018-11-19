e6581a25b5b1c1b86360247373a222b850e9a0cd
Tristanas <43069390+Tristanas@users.noreply.github.com>
2018-11-19 21:15:29 +0200

    First Controller
    Also removed some placeholders and made the webservice respond with JSON format.

--- WebService/App_Start/WebApiConfig.cs
+++ WebService/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebService
@@ -13,7 +14,8 @@ namespace WebService
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+              .Add(new MediaTypeHeaderValue("text/html"));
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ WebService/Controllers/CamerasController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SafePlace.Models;
+using SafePlace.Service;
+namespace WebService.Controllers
+{
+    public class CamerasController : ApiController
+    {
+        ICameraService service = new CameraService();
+        // GET: api/Cameras
+        public IEnumerable<Camera> Get()
+        {
+            return service.GetAllCameras();
+        }
+
+        // GET: api/Cameras/5
+        public IHttpActionResult Get(Guid id)
+        {
+            var cameras = service.GetAllCameras();
+            Camera cam = cameras.FirstOrDefault(camera => camera.Guid == id);
+            if (cam == null) return NotFound();
+            return Ok(cam);
+        }
+
+        // POST: api/Cameras
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Cameras/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Cameras/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
