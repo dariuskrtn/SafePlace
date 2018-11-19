010195a478801a3a9da150b09db35f9b79401abd
Tristanas <43069390+Tristanas@users.noreply.github.com>
2018-11-19 23:21:46 +0200

    Created ImageController
    Currently it has 1 method, which has hard coded uri inside. Wasn't sure how to write the uri parameter for the get function while testing.

---
+++ WebService/Controllers/ImagesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebService.Controllers
+{
+    public class ImagesController : ApiController
+    {
+        //jei debug paleidi ir parašai: http://localhost:54507/api/images, gauni byte kodą nuotraukos iš App_Data.
+        public string Get()
+        {
+            var root = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            //Uri = "App_Data/Koala.jpg" ar kitoks string, atitinkantis nuotrauka siame projekte.
+            var path = Path.Combine(root, "App_Data/when-you-study.jpg");
+
+            var bytes = File.ReadAllBytes(path);
+            var base64 = Convert.ToBase64String(bytes);
+
+            return "data:image/jpeg;base64," + base64;
+        }
+    }
+}
